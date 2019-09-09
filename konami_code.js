16a88766b500998f32f89126dbd7fee96b85764e
Nate Tan <nktan93@gmail.com>
2019-09-09 21:46:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const body = document.getElementsByTagName('body')
+  const body = document.getElementsByTagName('body');
+  body.addEventListener('keydown', onKeyDownHandler());
 
   // Keep track of index outside of the event handler.
   let index = 0;
